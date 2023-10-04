ec74d5474db92c512de8a1ad2611c9eb628e4db2
Daniel Reszka <dannn.r@gmail.com>
2023-10-04 11:06:06 +0200

    update page and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,33 +14,10 @@
     <!-- App Bar -->
     <AppBar>
       <svelte:fragment slot="lead">
-        <strong class="text-xl uppercase">Skeleton</strong>
+        <strong class="text-xl uppercase">Pigg.gy</strong>
       </svelte:fragment>
       <svelte:fragment slot="trail">
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://discord.gg/EXqV7W8MtY"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Discord
-        </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://twitter.com/SkeletonUI"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Twitter
-        </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://github.com/skeletonlabs/skeleton"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
+        <button class="btn btn-sm variant-ghost-surface"> Profile </button>
       </svelte:fragment>
     </AppBar>
   </svelte:fragment>
