d723f0029fc2f29f6b4164fb4900cc20efb669a3
Daniel Reszka <dannn.r@gmail.com>
2023-10-06 01:04:15 +0200

    add basic layers

--- src/routes/draw/+page.svelte
+++ src/routes/draw/+page.svelte
@@ -5,7 +5,20 @@
   import { HSLAdjustment } from './canvas'
   import { writable } from 'svelte/store'
 
-  let canvas: HTMLCanvasElement
+  let canvas1: HTMLCanvasElement
+  let canvas2: HTMLCanvasElement
+
+  const layer1 = writable({
+    visible: true,
+    name: 'Layer 1',
+  })
+
+  const layer2 = writable({
+    visible: true,
+    name: 'Layer 2',
+  })
+
+  let layerActive = layer1
 
   const WIDTH = 12
   const HEIGHT = 12
@@ -23,43 +36,55 @@
     drawing = false
   }
 
-  function paint(event: MouseEvent, ctx: CanvasRenderingContext2D | null) {
+  function paint(event: MouseEvent) {
     if (!drawing) return
+
+    const canvas = $layerActive.name === 'Layer 1' ? canvas1 : canvas2
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    if (!ctx) return
+
     const rect = canvas.getBoundingClientRect()
     const x = Math.floor((event.clientX - rect.left) / PIXEL_SIZE)
     const y = Math.floor((event.clientY - rect.top) / PIXEL_SIZE)
-    if (!ctx) return
     ctx.fillStyle = color
     ctx.fillRect(x, y, 1, 1)
   }
 
-  function draw(event: MouseEvent, ctx: CanvasRenderingContext2D | null) {
+  function draw(event: MouseEvent) {
+    const canvas = $layerActive.name === 'Layer 1' ? canvas1 : canvas2
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    if (!ctx) return
+
     const rect = canvas.getBoundingClientRect()
     const x = Math.floor((event.clientX - rect.left) / PIXEL_SIZE)
     const y = Math.floor((event.clientY - rect.top) / PIXEL_SIZE)
-    if (!ctx) return
     ctx.fillStyle = color
     ctx.fillRect(x, y, 1, 1)
   }
-
-  let ctx: CanvasRenderingContext2D
 
   function huePlus() {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const canvas = $layerActive.name === 'Layer 1' ? canvas1 : canvas2
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    if (!ctx) return
+    const imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height)
     const adjustedImageData = HSLAdjustment(imageData, 5, 0, 0)
     ctx.putImageData(adjustedImageData, 0, 0)
   }
 
   function hueMinus() {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const canvas = $layerActive.name === 'Layer 1' ? canvas1 : canvas2
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    if (!ctx) return
+    const imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height)
     const adjustedImageData = HSLAdjustment(imageData, -5, 0, 0)
     ctx.putImageData(adjustedImageData, 0, 0)
   }
 
   onMount(() => {
-    canvas.width = WIDTH
-    canvas.height = HEIGHT
-    ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+    canvas1.width = WIDTH
+    canvas1.height = HEIGHT
+    canvas2.width = WIDTH
+    canvas2.height = HEIGHT
 
     // var canvas2 = document.createElement('canvas')
     // var ctx2 = canvas.getContext('2d') as CanvasRenderingContext2D
@@ -83,16 +108,56 @@
         <button class="" on:click={huePlus}>+</button>
       </div>
     </section>
+
+    <hr />
+
+    <section
+      class="grid grid-cols-[1fr_auto_auto] gap-x-4 font-semibold text-surface-500 items-center"
+    >
+      <div class="contents">
+        <input
+          class="p-0 bg-transparent border-none hover:text-surface-300 transition-colors"
+          type="text"
+          bind:value={$layer2.name}
+        />
+        <input class="radio" type="radio" bind:group={layerActive} value={layer2} />
+        <input class="checkbox" type="checkbox" bind:checked={$layer2.visible} />
+      </div>
+      <div class="contents">
+        <input
+          class="p-0 bg-transparent border-none hover:text-surface-300 transition-colors"
+          type="text"
+          bind:value={$layer1.name}
+        />
+        <input class="radio" type="radio" bind:group={layerActive} value={layer1} />
+        <input class="checkbox" type="checkbox" bind:checked={$layer1.visible} />
+      </div>
+    </section>
   </aside>
-  <canvas
-    bind:this={canvas}
-    style="width:{WIDTH * PIXEL_SIZE}px; height:{HEIGHT * PIXEL_SIZE}px;"
-    use:clickOutside={stopDrawing}
-    on:mousedown={startDrawing}
-    on:mouseup={stopDrawing}
-    on:mousemove={(e) => paint(e, ctx)}
-    on:click={(e) => draw(e, ctx)}
-  />
+
+  <div class="stack">
+    <canvas
+      class:hidden={!$layer1.visible}
+      bind:this={canvas1}
+      style="width:{WIDTH * PIXEL_SIZE}px; height:{HEIGHT * PIXEL_SIZE}px;"
+      use:clickOutside={stopDrawing}
+      on:mousedown={startDrawing}
+      on:mouseup={stopDrawing}
+      on:mousemove={(e) => paint(e)}
+      on:click={(e) => draw(e)}
+    />
+
+    <canvas
+      class:hidden={!$layer2.visible}
+      bind:this={canvas2}
+      style="width:{WIDTH * PIXEL_SIZE}px; height:{HEIGHT * PIXEL_SIZE}px;"
+      use:clickOutside={stopDrawing}
+      on:mousedown={startDrawing}
+      on:mouseup={stopDrawing}
+      on:mousemove={(e) => paint(e)}
+      on:click={(e) => draw(e)}
+    />
+  </div>
 </div>
 
 <style lang="postcss">
@@ -100,5 +165,15 @@
     image-rendering: pixelated;
     image-rendering: crisp-edges;
     @apply bg-surface-100/10;
+  }
+
+  .stack {
+    @apply grid;
+  }
+  .stack > * {
+    grid-row-start: 0;
+    grid-row-end: 1;
+    grid-column-start: 0;
+    grid-column-end: 1;
   }
 </style>
