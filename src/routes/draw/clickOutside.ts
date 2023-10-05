e1e3ee6808c076379124600eb1c03ba2dfe83b29
Daniel Reszka <dannn.r@gmail.com>
2023-10-06 00:21:23 +0200

    add action clickOutside

---
+++ src/routes/draw/clickOutside.ts
@@ -0,0 +1,17 @@
+export function clickOutside(element: HTMLElement, callbackFunction: () => void) {
+  function onClick(event: MouseEvent) {
+    if (element.contains(event.target as HTMLElement)) return
+    callbackFunction()
+  }
+
+  document.body.addEventListener('click', onClick)
+
+  return {
+    update(newCallbackFunction: () => void) {
+      callbackFunction = newCallbackFunction
+    },
+    destroy() {
+      document.body.removeEventListener('click', onClick)
+    },
+  }
+}
