7b1848b4dbf3f25297d7c84224cdf17246e431f6
Daniel Reszka <dannn.r@gmail.com>
2023-10-10 17:46:25 +0200

    /paperjs: add with experiment

--- src/routes/paperjs/+page.svelte
+++ src/routes/paperjs/+page.svelte
@@ -1,5 +1,54 @@
 <script lang="ts">
-  import type { PageData } from './$types'
+  import paper, { Path } from 'paper'
+  import { onMount } from 'svelte'
 
-  export let data: PageData
+  let svg = ''
+
+  onMount(() => {
+    const canvas = document.getElementById('myCanvas') as HTMLCanvasElement | null
+    console.log(canvas)
+    if (!canvas) return
+    paper.setup(canvas)
+
+    const rect1 = new paper.Path.Rectangle({ point: [0, 0], size: [30, 30], fillColor: 'blue' })
+    const rect2 = new paper.Path.Rectangle({ point: [30, 0], size: [30, 30], fillColor: 'green' })
+    const rect3 = new paper.Path.Rectangle({ point: [30, 30], size: [30, 30], fillColor: 'green' })
+    const rect4 = new paper.Path.Rectangle({ point: [30, 30], size: [30, 30], fillColor: 'green' })
+
+    console.log(rect1.fillColor)
+    console.log(rect2.fillColor)
+    console.log(rect3.fillColor)
+
+    const merged = rect1.unite(rect3) //.unite(rect3).subtract(rect4)
+
+    merged.selected = true
+    // rect1.selected = true
+    // rect2.selected = true
+    // rect3.selected = true
+    // rect4.selected = true
+
+    rect1.remove()
+    rect2.remove()
+    rect3.remove()
+    rect4.remove()
+    // merged.remove()
+
+    svg = paper.project.exportSVG({ asString: true }) as string
+  })
 </script>
+
+<canvas id="myCanvas" />
+
+<p>{JSON.stringify(svg, null, 2)}</p>
+
+{#if svg}
+  {@html svg}
+{/if}
+
+<style lang="postcss">
+  #myCanvas {
+    @apply border border-white/30 bg-white/5 my-16 mx-auto;
+    width: 400px;
+    height: 400px;
+  }
+</style>
