c5c9afa5387f110bc852fc482d3a0df069b79fc4
Daniel Reszka <dannn.r@gmail.com>
2023-10-12 23:28:10 +0200

    TraitsList: improve: change to tabular view

--- src/routes/tagtree2/Persons.svelte
+++ src/routes/tagtree2/Persons.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import { Ratings } from '@skeletonlabs/skeleton'
-  import { getPersonsContext, setPersonsContext } from './usePersons'
   import TraitsList from './TraitsList.svelte'
+  import { getPersonsContext, setPersonsContext } from './usePersons'
 
   const initPersons = [
     { id: 'tom', name: 'Tom' },
@@ -14,28 +13,20 @@
     { kind: 'expertise', personId: 'tom', nodeId: '02', body: 'pro 02', scale: 5 },
     { kind: 'interest', personId: 'jill', nodeId: '01', body: '01 is ok', scale: 4 },
     { kind: 'expertise', personId: 'jill', nodeId: '02', body: 'newbie 02', scale: 1 },
+    { kind: 'hobby', personId: 'jill', nodeId: '02', body: 'hobby 02', scale: 4 },
   ]
 
   setPersonsContext(initPersons, initTraits)
   const { persons, traits } = getPersonsContext()
 </script>
 
-<h1>Persons</h1>
-<div class="space-y-2">
-  {#each $persons as person}
-    <div class="p-2">
+<div class="flex flex-wrap">
+  {#each [...$persons, ...$persons, ...$persons] as person}
+    <div class="min-w-[50%] flex-grow p-2">
       <h2>{person.name}</h2>
 
-      <div class="grid grid-cols-2">
-        <div class="p-2 variant-soft">
-          <h3>Interest</h3>
-          <TraitsList {person} traitsKind="interest" />
-        </div>
-
-        <div class="p-2 variant-soft">
-          <h3>Expertise</h3>
-          <TraitsList {person} traitsKind="expertise" />
-        </div>
+      <div class="variant-soft rounded">
+        <TraitsList {person} />
       </div>
     </div>
   {/each}
--- src/routes/tagtree2/TraitsList.svelte
+++ src/routes/tagtree2/TraitsList.svelte
@@ -1,42 +1,73 @@
 <script lang="ts">
   import { Ratings } from '@skeletonlabs/skeleton'
   import * as Icons from './Icons'
-  import { getPersonsContext, type Person, type Trait } from './usePersons'
+  import { getPersonsContext, type Person } from './usePersons'
+  import { getTreeContext } from './useTree'
+  import { onlyUnique } from './utils/array'
+  import { upperFirst } from './utils/string'
 
   const { traits } = getPersonsContext()
+  const { nodes } = getTreeContext()
+
   export let person: Person
-  export let traitsKind: Trait['kind']
+
+  $: personTraits = $traits.filter((t) => t.personId === person.id)
+  $: traitsKind = personTraits.map((t) => t.kind).filter(onlyUnique)
+  $: traitNodeIds = personTraits.map((t) => t.nodeId).filter(onlyUnique)
+
+  $: colsCount = personTraits.map((t) => t.kind).filter(onlyUnique).length + 1
 </script>
 
-{#each $traits.filter((t) => t.personId === person.id && t.kind === traitsKind) as trait}
-  <div class="flex gap-2">
-    <div class="grid gap-2 grid-cols-2">
-      <span>{trait.nodeId}</span>
+<div class="grid" style:--colsCount={colsCount}>
+  <div class="th">Node</div>
+  {#each traitsKind as kind}
+    <div class="th">{upperFirst(kind)}</div>
+  {/each}
 
-      <div class="flex gap-2 items-center">
-        <div class="opacity-75 hover:opacity-100">
-          <Ratings
-            bind:value={trait.scale}
-            max={5}
-            interactive
-            spacing="gap-1"
-            on:icon={(e) => (trait.scale = e.detail.index)}
-          >
-            <svelte:fragment slot="empty"><Icons.SquareEmpty /></svelte:fragment>
-            <svelte:fragment slot="half"><Icons.SquareHalved /></svelte:fragment>
-            <svelte:fragment slot="full"><Icons.Square /></svelte:fragment>
-          </Ratings>
+  {#each traitNodeIds as nodeId}
+    <div>{nodes.findById(nodeId).value}</div>
+    {#each traitsKind as kind}
+      <div class="flex items-center">
+        <div class="opacity-75 hover:opacity-100 flex items-center gap-2">
+          {#each $traits.filter((t) => t.personId === person.id && t.nodeId === nodeId && t.kind === kind) as trait}
+            <Ratings
+              bind:value={trait.scale}
+              max={5}
+              interactive
+              spacing="gap-0.5"
+              on:icon={(e) => (trait.scale = e.detail.index)}
+            >
+              <svelte:fragment slot="empty"><Icons.SquareEmpty /></svelte:fragment>
+              <svelte:fragment slot="half"><Icons.SquareHalved /></svelte:fragment>
+              <svelte:fragment slot="full"><Icons.Square /></svelte:fragment>
+            </Ratings>
+            <button
+              class="opacity-25 hover:opacity-100"
+              on:click={() =>
+                confirm(`Are you sure you want to remove this ${trait.kind} for ${person.name}?`) &&
+                traits.remove(trait)}
+            >
+              <Icons.Xmark />
+            </button>
+          {:else}
+            -
+          {/each}
         </div>
-
-        <button
-          class="opacity-25 hover:opacity-100"
-          on:click={() =>
-            confirm(`Are you sure you want to remove this ${traitsKind} for ${person.name}?`) &&
-            traits.remove(trait)}
-        >
-          <Icons.Xmark />
-        </button>
       </div>
-    </div>
-  </div>
-{/each}
+    {/each}
+  {/each}
+</div>
+
+<style lang="postcss">
+  .grid {
+    grid-template-columns: 1fr repeat(calc(var(--colsCount) - 1), auto);
+    @apply px-2 py-1;
+  }
+  .grid > div {
+    @apply px-4 py-1;
+  }
+
+  .th {
+    @apply font-bold text-surface-500;
+  }
+</style>
