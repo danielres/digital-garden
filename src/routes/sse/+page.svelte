0abeb8c1044b05cb47a2ef793a672b89344418d0
Daniel Reszka <dannn.r@gmail.com>
2023-10-15 22:24:46 +0200

    sse: improve UI + add explanations

--- src/routes/sse/+page.svelte
+++ src/routes/sse/+page.svelte
@@ -24,11 +24,33 @@
   }
 </script>
 
-<textarea class="textarea" bind:value={newMessage} />
-<button on:click|preventDefault={sendMessage}>Send</button>
+<div class="max-w-2xl mx-auto my-8 space-y-8">
+  <div class="variant-soft rounded p-4 text-sm space-y-4">
+    <p>
+      This demonstrates how to use SSE (server-side-events) + the Stream API as an alternative to
+      websockets or similar. This approach is simpler and suitable for smaller projects.
+    </p>
+    <p>Open in 2 different browser windows to see the realtime updates.</p>
+  </div>
 
-<ul class="border p-4">
-  {#each messages as message (message.content)}
-    <li>{message.content}</li>
-  {/each}
-</ul>
+  <div class="grid grid-cols-2 gap-8">
+    <div class="">
+      <h2>New message</h2>
+      <textarea class="textarea rounded" bind:value={newMessage} />
+      <button class="btn variant-ghost mt-4 rounded" on:click|preventDefault={sendMessage}>
+        Send message
+      </button>
+    </div>
+
+    <div>
+      <h2>Messages</h2>
+      <div class="border p-4 rounded">
+        <ul class="list-disc ml-8">
+          {#each messages as message (message.content)}
+            <li>{message.content}</li>
+          {/each}
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
