cb1a48a901a5f5b9ef166554adad697752a65cc6
Daniel Reszka <dannn.r@gmail.com>
2023-10-19 10:01:35 +0200

    data: nodes: use realistic contents

--- src/routes/tagtree2/data.ts
+++ src/routes/tagtree2/data.ts
@@ -2,29 +2,47 @@ import type { Person, Trait } from './usePersons'
 import type { Edge, Node } from './useTree'
 
 export const persons: Person[] = [
-  { id: 'tom', name: 'Tom', body: 'I am a person and I love cats.' },
+  { id: 'daniel', name: 'Daniel', body: 'I am a person and I love cats.' },
   { id: 'jill', name: 'Jill', body: 'I am a person and I love dogs.' },
   { id: 'paul', name: 'Paul', body: 'I am a person and I love hamsters.' },
 ]
 
 export const traits: Trait[] = [
-  { kind: 'hobby', personId: 'jill', nodeId: '02', body: 'hobby 02', scale: 4 },
-  { kind: 'interest', personId: 'tom', nodeId: '01', body: '01 is great', scale: 5 },
-  { kind: 'interest', personId: 'tom', nodeId: '02', body: '02 is meh', scale: 3.5 },
-  { kind: 'expertise', personId: 'tom', nodeId: '02', body: 'pro 02', scale: 5 },
-  { kind: 'interest', personId: 'jill', nodeId: '01', body: '01 is ok', scale: 4 },
-  { kind: 'expertise', personId: 'jill', nodeId: '02', body: 'newbie 02', scale: 1 },
+  { kind: 'hobby', personId: 'jill', nodeId: 'svelte', body: 'hobby svelte', scale: 4 },
+  {
+    kind: 'interest',
+    personId: 'daniel',
+    nodeId: 'typescript',
+    body: 'typescript is great',
+    scale: 5,
+  },
+  { kind: 'interest', personId: 'daniel', nodeId: 'svelte', body: 'svelte is meh', scale: 3.5 },
+  { kind: 'expertise', personId: 'daniel', nodeId: 'svelte', body: 'pro svelte', scale: 5 },
+  { kind: 'interest', personId: 'jill', nodeId: 'typescript', body: 'typescript is ok', scale: 4 },
+  { kind: 'expertise', personId: 'jill', nodeId: 'svelte', body: 'newbie svelte', scale: 1 },
 ]
 
 export const nodes: Node[] = [
   { value: 'root', id: 'root', body: '' },
-  { value: 'node1', id: '01', body: 'Node1 body' },
-  { value: 'node2 Lorem', id: '02', body: 'Node2 body' },
-  { value: 'node3', id: '03', body: 'Node3 body' },
+  {
+    value: 'Typescript',
+    id: 'typescript',
+    body: 'TypeScript is a superset of JavaScript that adds optional static typing.',
+  },
+  {
+    value: 'Svelte',
+    id: 'svelte',
+    body: `Svelte: A framework for building web user interfaces using components. <br />SvelteKit: A framework built on Svelte for creating web applications with routing, server-side rendering, and other features.`,
+  },
+  {
+    value: 'Sveltekit',
+    id: 'sveltekit',
+    body: 'Svelte: A framework for building web user interfaces using components. <br />SvelteKit: A framework built on Svelte for creating web applications with routing, server-side rendering, and other features.',
+  },
 ]
 
 export const edges: Edge[] = [
-  { parentId: 'root', childId: '01' },
-  { parentId: 'root', childId: '02' },
-  { parentId: '02', childId: '03' },
+  { parentId: 'root', childId: 'typescript' },
+  { parentId: 'root', childId: 'svelte' },
+  { parentId: 'svelte', childId: 'sveltekit' },
 ]
