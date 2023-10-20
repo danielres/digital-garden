26c007143a326c505a78393c033b3df275869eb5
Daniel Reszka <dannn.r@gmail.com>
2023-10-20 13:37:04 +0200

    /xstate-counter: improve experiment

--- src/routes/xstate-counter/+page.svelte
+++ src/routes/xstate-counter/+page.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
-  import { useMachine } from '@xstate/svelte'
+  import { useMachine, useSelector } from '@xstate/svelte'
   import { counterMachine } from './machine'
 
   const { state, send, service } = useMachine(counterMachine)
+  const count = useSelector(service, (state) => state.context.count)
+  let step = 1
 </script>
 
 <div class="max-w-md mx-auto py-16 grid justify-items-center gap-8">
   <button
     class="btn variant-ghost p-2 text-center"
-    on:click={() => send($service.value === 'active' ? 'DEACTIVATE' : 'ACTIVATE')}
+    on:click={() => send($state.matches('active') ? 'OFF' : 'ON')}
   >
-    {$service.value}
+    {$state.matches('active') ? 'ON' : 'OFF'}
   </button>
 
-  <div class="w-16 text-center">{$state.context.count}</div>
-
-  <div class="flex gap-4">
-    <button class="btn variant-ghost" on:click={() => send('DEC')}>DEC</button>
-    <button class="btn variant-ghost" on:click={() => send('INC')}>INC</button>
+  <div class="w-16 text-center">
+    {$count}
   </div>
+
+  {#if $state.matches('active')}
+    <div class="flex gap-4">
+      {#each ['DEC', 'INC'] as command}
+        <button class="btn variant-ghost" on:click={() => send(command, { step })}>
+          {command}
+        </button>
+      {/each}
+    </div>
+
+    <div>
+      <button class="btn btn-icon variant-ghost" on:click={() => step--}>-</button>
+      {step}
+      <button class="btn btn-icon variant-ghost" on:click={() => step++}>+</button>
+    </div>
+  {/if}
 </div>
--- src/routes/xstate-counter/machine.ts
+++ src/routes/xstate-counter/machine.ts
@@ -1,30 +1,49 @@
-import { assign, createMachine } from 'xstate'
+import { assign, createMachine, type EventObject } from 'xstate'
 
 export const counterMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOgKwBdcA3MAYgEEBhAFQEkA1BlgUQG0ADAF1EoAA4B7WLioT8okAA9EAJgDMANhIqNAFjUBGAQbVqVKgJwAOKwBoQAT0Smr2gOy6VAVgECNArws3AF9g+zQsPEJSShp6ABEeZnYuXkERJBBJaVl5TOUEcy0VNwErA3KNL383EvsnQrc3EgM3Cq8dXQsfANDwjBwCYhJY2jo2ADkmdIVsmVw5BQKAWi9mjQ0DXV1jNQtPa116xAM1kl0DLZUrNo6-XSs+kAjB6JHMKjHE6eFZqXnFvkTgZihYNBY1F5TEY3F5dG5jghTs0LlcbqcVPcvKEwiB8BIIHAFC8osQ-jkFnlQCsjF4SBstjsTPtLFYjo5EKUSGUrF4MSDwSotk8SUNSOQPnFyQCqUpEPpXAILlUbgJriptoiSlorKZDG41Lp-PpsbjRW9RmBpbklvKERzGjq9a1DcbITjgkA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOgKwBdcA3MAYgHkA5AbQAYBdRUABwHtYuKn3zcQAD0QBaAMwBWEvICMcgGxKAnDIAcStgCY22gDQgAnogDsGkktX25c-fO2HVAX3em0WPIVKUNPQMAGIh7FxIIPyCwqJRkggyACwk+to6ySpZ2qpOKqYWCCr6ijLlyfpZGpYyBjKe3hg4BMQkgbQkmHwArvhU+FB0AJJMAMIRYjFCuCJiifpySmkyqhpy8mqWyU4m5oj6qjK26smqydrr+ov6lo0gPi3+7ZhUnd19A0MAIgCiE5wpgIZnMEogZJZSo5DNtnOkjrVCgcjiclGcLlcbnd7vg+BA4GJHn5iEDYrN4qBElI9JYSNpXLpsrp6ckkQgpNpbGpFq5VIctJYlEp7kTWqRyK8gqSQRSJNI5DZ6ekhXILkoWWz9EpSuUKlVNLV6iLmsSApLaNK4vMDmw2GlkrVIXVVfJFmyhal1c42JorskjB4vA8TWKXm8wF1ev0CFBLeTrQgqpyqo7vS6nHJNbaSKpBfSUjtnM7PJ4gA */
   predictableActionArguments: true,
-  initial: 'active',
   context: {
     count: 0,
   },
+  initial: 'active',
   states: {
     inactive: {
       on: {
-        ACTIVATE: {
+        ON: {
           target: 'active',
         },
       },
     },
+
     active: {
+      initial: 'counting',
+
       on: {
-        DEACTIVATE: {
-          target: 'inactive',
-        },
-        INC: {
-          actions: assign({ count: (context) => context.count + 1 }),
-        },
-        DEC: {
-          actions: assign({ count: (context) => context.count - 1 }),
+        OFF: 'inactive',
+      },
+
+      states: {
+        counting: {
+          on: {
+            INC: {
+              target: 'counting',
+              internal: true,
+              actions: assign({
+                count: (context, event: EventObject & { step: number }) =>
+                  context.count + event.step,
+              }),
+            },
+
+            DEC: {
+              target: 'counting',
+              internal: true,
+              actions: assign({
+                count: (context, event: EventObject & { step: number }) =>
+                  context.count - event.step,
+              }),
+            },
+          },
         },
       },
     },
