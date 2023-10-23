b64234dbfe47db712eba1f8a0d980345e926418a
Daniel Reszka <dannn.r@gmail.com>
2023-10-23 09:22:07 +0200

    NodeTree: extract MenuModes.svelte

--- src/routes/tagtree2/+layout.svelte
+++ src/routes/tagtree2/+layout.svelte
@@ -2,6 +2,7 @@
   import { goto } from '$app/navigation'
   import { page } from '$app/stores'
   import Tree from './NodeTree.svelte'
+  import MenuModes from './NodeTree/MenuModes.svelte'
   import Panel from './Panel.svelte'
   import Persons from './Persons.svelte'
   import * as data from './data'
@@ -11,7 +12,7 @@
   setPersonsContext(data.persons, data.traits)
   setTreeContext(data.nodes, data.edges)
 
-  const { mode, nodes } = getTreeContext()
+  const { nodes } = getTreeContext()
 </script>
 
 {#if $page.url.pathname !== '/tagtree2'}
@@ -29,43 +30,10 @@
     <h2>Topics</h2>
 
     <div class="space-y-4 variant-soft p-4">
-      <div class="flex gap-4">
-        <label>
-          <input
-            class="radio"
-            type="radio"
-            value={'move'}
-            on:click={() => ($mode = { type: 'move' })}
-            checked={$mode.type === 'move'}
-          />
-          <span>Move</span>
-        </label>
-
-        <label>
-          <input
-            class="radio"
-            type="radio"
-            on:click={() => ($mode = { type: 'copy' })}
-            checked={$mode.type === 'copy'}
-          />
-          <span>Copy</span>
-        </label>
-
-        <label>
-          <input
-            class="radio"
-            type="radio"
-            on:click={() => ($mode = { type: 'add' })}
-            checked={$mode.type.startsWith('add')}
-          />
-          <span>Add</span>
-        </label>
-      </div>
+      <MenuModes />
 
       <Tree
-        on:newNode={({ detail: { value, parentId } }) => {
-          nodes.add({ value, parentId })
-        }}
+        on:newNode={({ detail: { value, parentId } }) => nodes.add({ value, parentId })}
         on:nodeClicked={({ detail: nodeId }) => goto(`/tagtree2/topics/${nodeId}`)}
       />
     </div>
