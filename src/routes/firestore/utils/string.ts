5457188fe14f6ccd3bdcf33575f148a9b209871a
Daniel Reszka <dannn.r@gmail.com>
2023-10-31 20:03:13 +0100

    utils/string: add slugify + sanitizeFormInput

--- src/routes/firestore/utils/string.ts
+++ src/routes/firestore/utils/string.ts
@@ -6,3 +6,17 @@ export function truncate(str: string, maxLength = 20) {
 export function upperFirst(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
+
+
+export function slugify(string: string) {
+  return string
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)+/g, '')
+}
+
+export function sanitizeFormInput(str: string){
+ return upperFirst(str.replace(/\s+/g, ' ').trim())
+}
