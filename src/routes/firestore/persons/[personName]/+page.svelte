0351edba25c5d4ae5eabc49c97f3691d15980dc3
Daniel Reszka <dannn.r@gmail.com>
2023-11-01 10:23:20 +0100

    person: form: add support for traits

--- src/routes/firestore/persons/[personName]/+page.svelte
+++ src/routes/firestore/persons/[personName]/+page.svelte
@@ -2,9 +2,10 @@
   import { page } from '$app/stores'
   import { Avatar } from '@skeletonlabs/skeleton'
   import { groupByKey } from '../../../tagtree2/utils/object'
-  import { getAppContext, type Person } from '../../appContext'
+  import { getAppContext, type Trait } from '../../appContext'
   import FormEditableDoc from '../../components/FormEditableDoc.svelte'
   import Markdown from '../../components/Markdown.svelte'
+  import { extractNestedInputs } from '../../utils/forms'
   import { paths } from '../../utils/navigation'
   import { upperFirst } from '../../utils/string'
 
@@ -17,9 +18,18 @@
     'kind'
   )
 
-  const handleUpdate = (values: Partial<Person>) => {
+  const handleUpdate = (values: Record<string, string>) => {
     if (!person) return
-    persons.update(person.id, values)
+    const { traits: traitsValues, ...personValues } = extractNestedInputs(values)
+
+    Object.entries(traitsValues).forEach(([id, values]) => {
+      const { text, scale } = values as Partial<Trait>
+      const existingTrait = $traits.find((t) => t.id === id)
+      if (existingTrait?.text === text && existingTrait?.scale === scale) return
+      traits.update(id, { text, scale })
+    })
+
+    persons.update(person.id, personValues)
   }
 </script>
 
@@ -31,7 +41,7 @@
         {person.name}
       </div>
 
-      <div slot="fields">
+      <svelte:fragment slot="fields">
         <label>
           <span>Picture url</span>
           <input type="text" class="textarea" name="picture" value={person.picture ?? ''} />
@@ -41,7 +51,41 @@
           <span>Description</span>
           <textarea class="textarea" name="body">{person.body ?? ''}</textarea>
         </label>
-      </div>
+
+        <fieldset name="traits">
+          {#each Object.entries(personTraits) as [kind, traits]}
+            <div>
+              <h3>{upperFirst(kind)}</h3>
+
+              <ul class="grid gap-1">
+                {#each traits as trait}
+                  {@const topic = $topics.find((t) => trait.topicId === t.id)}
+
+                  {#if topic}
+                    <li class="px-4 py-2 variant-soft rounded-md grid gap-2">
+                      <div class="flex gap-2">
+                        <a class="clickable" href={paths.topics(topic.name)}>{topic.name}</a>
+                        :
+                        <input
+                          class="textarea p-0 w-4 text-center"
+                          type="text"
+                          name="traits[{trait.id}][scale]"
+                          value={trait.scale}
+                        />
+                      </div>
+
+                      <div>
+                        <!-- prettier-ignore -->
+                        <textarea class="textarea" name="traits[{trait.id}][text]">{trait.text ?? ''}</textarea>
+                      </div>
+                    </li>
+                  {/if}
+                {/each}
+              </ul>
+            </div>
+          {/each}
+        </fieldset>
+      </svelte:fragment>
 
       <div slot="content" class="grid gap-4">
         {#if person.body}
@@ -55,7 +99,7 @@
             <h3>{upperFirst(kind)}</h3>
 
             <ul class="grid gap-1">
-              {#each traits as trait, i}
+              {#each traits as trait}
                 {@const topic = $topics.find((t) => trait.topicId === t.id)}
 
                 {#if topic}
