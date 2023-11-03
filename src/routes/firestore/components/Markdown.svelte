0a712b2e1114645b7d3fccebbb62df17ecb09d94
Daniel Reszka <dannn.r@gmail.com>
2023-11-03 11:16:24 +0100

    Markdown: do not render on empty text

--- src/routes/firestore/components/Markdown.svelte
+++ src/routes/firestore/components/Markdown.svelte
@@ -18,34 +18,36 @@
   const md = new MarkdownIt()
 </script>
 
-{#if plainText}
-  {truncate(stripHtml(md.render(String(text ?? ''))), 100)}
-{:else}
-  {#key text}
-    <div>
-      <div
-        class="markdown overflow-y-auto variant-glass px-4 py-2"
-        class:max-h-[9rem]={!expanded}
-        class:max-h-[36rem]={expanded}
-        bind:this={collapsibleEl}
-      >
-        {@html md.render(String(text ?? ''))}
-      </div>
-
-      {#if isOverflowing}
-        <button
-          on:click={() => (expanded = !expanded)}
-          class="w-full flex justify-center variant-glass"
-          type="button"
+{#if text}
+  {#if plainText}
+    {truncate(stripHtml(md.render(String(text ?? ''))), 100)}
+  {:else}
+    {#key text}
+      <div>
+        <div
+          class="markdown overflow-y-auto variant-glass px-4 py-2"
+          class:max-h-[9rem]={!expanded}
+          class:max-h-[36rem]={expanded}
+          bind:this={collapsibleEl}
         >
-          <div class="px-2">
-            <svelte:component
-              this={expanded ? Icons.ChevronUp : Icons.ChevronDown}
-              class="w-5 opacity-50 hover:opacity-100"
-            />
-          </div>
-        </button>
-      {/if}
-    </div>
-  {/key}
+          {@html md.render(String(text ?? ''))}
+        </div>
+
+        {#if isOverflowing}
+          <button
+            on:click={() => (expanded = !expanded)}
+            class="w-full flex justify-center variant-glass"
+            type="button"
+          >
+            <div class="px-2">
+              <svelte:component
+                this={expanded ? Icons.ChevronUp : Icons.ChevronDown}
+                class="w-5 opacity-50 hover:opacity-100"
+              />
+            </div>
+          </button>
+        {/if}
+      </div>
+    {/key}
+  {/if}
 {/if}
