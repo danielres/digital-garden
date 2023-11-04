0f79c454b893a94d4f23805f81440fff0d927c61
Daniel Reszka <dannn.r@gmail.com>
2023-11-05 00:56:58 +0100

    components/Panel: add

---
+++ src/routes/firestore/components/Panel.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let isAdmin = false
+</script>
+
+<div
+  class="p-4 h-fit rounded border bg-surface-500/20"
+  class:border-error-500={isAdmin}
+  class:border-surface-500={!isAdmin}
+>
+  <slot />
+</div>
