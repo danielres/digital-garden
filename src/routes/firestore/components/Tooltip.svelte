235e0cce62316e2a5e5609e6b0953d674d59cee9
Daniel Reszka <dannn.r@gmail.com>
2023-11-05 18:33:58 +0100

    components/Tooltip: add

---
+++ src/routes/firestore/components/Tooltip.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+  import { arrow, computePosition, flip, offset, shift, type Placement } from '@floating-ui/dom'
+
+  let functionToCall = () => {}
+
+  let tooltip: HTMLDivElement
+  let anchor: HTMLButtonElement
+  let arrowEl: HTMLElement
+
+  export let placement: Placement = 'right'
+  export let zindex = 100
+
+  const close = () => tooltip.classList.add('hidden')
+  const toggle = () => tooltip.classList.toggle('hidden')
+  const setAnchor = (el: HTMLElement) => (anchor = el as HTMLButtonElement)
+
+  function confirm(func: () => void) {
+    functionToCall = func
+    toggle()
+  }
+
+  function doCompute() {
+    computePosition(anchor, tooltip, {
+      placement,
+      middleware: [
+        //
+        offset(16),
+        flip(),
+        shift({ padding: 5 }),
+        arrow({ element: arrowEl }),
+      ],
+    }).then(({ x, y, placement, middlewareData }) => {
+      Object.assign(tooltip.style, { left: `${x}px`, top: `${y}px` })
+
+      const placementSide = placement.split('-')[0] as 'top' | 'right' | 'bottom' | 'left'
+
+      const staticSide = {
+        top: 'bottom',
+        right: 'left',
+        bottom: 'top',
+        left: 'right',
+      }[placementSide]
+
+      arrowEl.classList.remove('arrow-top', 'arrow-right', 'arrow-bottom', 'arrow-left')
+      arrowEl.classList.add(`arrow-${placementSide}`)
+
+      const arrowX = middlewareData.arrow?.x
+      const arrowY = middlewareData.arrow?.y
+
+      Object.assign(arrowEl.style, {
+        left: arrowX != null ? `${arrowX}px` : '',
+        top: arrowY != null ? `${Math.max(arrowY, 10)}px` : '',
+        right: '',
+        bottom: '',
+        [staticSide]: '-7px',
+      })
+    })
+  }
+
+  $: anchor && tooltip && doCompute()
+</script>
+
+<svelte:body on:scroll={() => anchor && tooltip && doCompute()} />
+<svelte:window on:resize={() => anchor && tooltip && doCompute()} />
+
+<slot {toggle} {setAnchor} {confirm} />
+
+<div
+  bind:this={tooltip}
+  class="rounded absolute text-error-600-300-token hidden bg-surface-800 shadow-lg"
+  style:z-index={zindex}
+>
+  <div class="variant-soft-error p-8 space-y-8 rounded border-2 border-error-500">
+    <slot name="description" />
+
+    <div class="buttons">
+      <button type="button" class="btn rounded variant-ghost-error" on:click={close}>Cancel</button>
+      <button type="button" class="btn rounded variant-filled-error" on:click={functionToCall}>
+        Confirm
+      </button>
+    </div>
+  </div>
+
+  <div class="arrow border-error-500" bind:this={arrowEl} style:z-index={50}>
+    <div class="bg-surface-800">
+      <div class="variant-soft-error w-[12px] aspect-square" />
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  .buttons {
+    @apply flex gap-4 justify-center;
+  }
+
+  .arrow {
+    position: absolute;
+    transform: rotate(45deg);
+  }
+
+  div :global(.arrow-left) {
+    @apply border-t-2 border-r-2;
+  }
+
+  div :global(.arrow-right) {
+    @apply border-b-2 border-l-2;
+  }
+
+  div :global(.arrow-top) {
+    @apply border-b-2 border-r-2;
+  }
+
+  div :global(.arrow-bottom) {
+    @apply border-t border-l;
+  }
+</style>
