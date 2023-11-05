10f39f83431ba89eba9f7792fbeb249218cbb5b7
Daniel Reszka <dannn.r@gmail.com>
2023-11-05 19:09:24 +0100

    Tooltip: improve with svelte transitions

--- src/routes/firestore/components/Tooltip.svelte
+++ src/routes/firestore/components/Tooltip.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
   import { arrow, computePosition, flip, offset, shift, type Placement } from '@floating-ui/dom'
+  import { focusTrap } from '@skeletonlabs/skeleton'
+  import { fade } from 'svelte/transition'
 
   let functionToCall = () => {}
 
   let tooltip: HTMLDivElement
   let anchor: HTMLButtonElement
   let arrowEl: HTMLElement
+  let isOpen = false
 
   export let placement: Placement = 'right'
   export let zindex = 100
 
-  const close = () => tooltip.classList.add('hidden')
-  const toggle = () => tooltip.classList.toggle('hidden')
+  const close = () => (isOpen = false)
+  const toggle = () => (isOpen = !isOpen)
   const setAnchor = (el: HTMLElement) => (anchor = el as HTMLButtonElement)
 
   function confirm(func: () => void) {
@@ -57,36 +60,51 @@
     })
   }
 
-  $: anchor && tooltip && doCompute()
+  $: if (anchor && tooltip) doCompute()
 </script>
 
-<svelte:body on:scroll={() => anchor && tooltip && doCompute()} />
 <svelte:window on:resize={() => anchor && tooltip && doCompute()} />
 
 <slot {toggle} {setAnchor} {confirm} />
 
-<div
-  bind:this={tooltip}
-  class="rounded absolute text-error-600-300-token hidden bg-surface-800 shadow-lg"
-  style:z-index={zindex}
->
-  <div class="variant-soft-error p-8 space-y-8 rounded border-2 border-error-500">
-    <slot name="description" />
+{#if isOpen}
+  <button
+    style:z-index={zindex}
+    in:fade={{ duration: 150 }}
+    out:fade={{ delay: 150, duration: 150 }}
+    type="button"
+    class="fixed inset-0 bg-surface-50-900-token w-full opacity-20"
+    on:click={close}
+  />
 
-    <div class="buttons">
-      <button type="button" class="btn rounded variant-ghost-error" on:click={close}>Cancel</button>
-      <button type="button" class="btn rounded variant-filled-error" on:click={functionToCall}>
-        Confirm
-      </button>
+  <div
+    style:z-index={zindex}
+    bind:this={tooltip}
+    use:focusTrap={true}
+    in:fade={{ duration: 150, delay: 150 }}
+    out:fade={{ duration: 150 }}
+    class="rounded absolute text-error-600-300-token bg-surface-800 shadow-lg"
+  >
+    <div class="variant-soft-error p-8 space-y-8 rounded border-2 border-error-500">
+      <slot name="description" />
+
+      <div class="buttons">
+        <button type="button" class="btn rounded variant-ghost-error" on:click={close}>
+          Cancel
+        </button>
+        <button type="button" class="btn rounded variant-filled-error" on:click={functionToCall}>
+          Confirm
+        </button>
+      </div>
+    </div>
+
+    <div class="arrow border-error-500" bind:this={arrowEl} style:z-index={50}>
+      <div class="bg-surface-800">
+        <div class="variant-soft-error w-[12px] aspect-square" />
+      </div>
     </div>
   </div>
-
-  <div class="arrow border-error-500" bind:this={arrowEl} style:z-index={50}>
-    <div class="bg-surface-800">
-      <div class="variant-soft-error w-[12px] aspect-square" />
-    </div>
-  </div>
-</div>
+{/if}
 
 <style lang="postcss">
   .buttons {
