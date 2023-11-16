0d2a0a906f535eaf85c1886fb852cc2dfade3783
Daniel Reszka <dannn.r@gmail.com>
2023-11-16 19:59:21 +0100

    Card: improve variants

--- src/routes/pocketbase/components/Card.svelte
+++ src/routes/pocketbase/components/Card.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import { twMerge } from 'tailwind-merge'
 
-  export let variant: 'info' | 'error' = 'info'
+  export let variant: 'primary' | 'error' | 'neutral' = 'neutral'
+  export let bg = true
 </script>
 
 <div
   class={twMerge(
     'card p-4',
-    'variant-glass-primary text-primary-700-200-token',
-    variant === 'error' && 'variant-glass-error text-error-700-200-token'
+    'variant-glass-surface text-surface-700-200-token',
+    variant === 'error' && 'variant-glass-error text-error-700-200-token',
+    variant === 'primary' && 'variant-glass-primary text-primary-700-200-token',
+    !bg && '!bg-transparent'
   )}
 >
   <slot />
