b9d0d068b6fe556e2358ae1fa6870492d40bcbf8
Daniel Reszka <dannn.r@gmail.com>
2023-11-16 21:32:29 +0100

    migrations: users: add slug

---
+++ pocketbase_0.19.4_linux_amd64/pb_migrations/1700165600_updated_users.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "nbys32f1",
+    "name": "slug",
+    "type": "text",
+    "required": true,
+    "presentable": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": "[a-z0-9]-"
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "nbys32f1",
+    "name": "slug",
+    "type": "text",
+    "required": true,
+    "presentable": true,
+    "unique": false,
+    "options": {
+      "min": 3,
+      "max": null,
+      "pattern": "[a-z0-9]-"
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
