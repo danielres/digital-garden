36ac3903605e7ddd8d15214ed89c3d97027397ac
Daniel Reszka <dannn.r@gmail.com>
2023-11-16 20:07:17 +0100

    Avatar: add

---
+++ src/routes/pocketbase/components/Avatar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Avatar } from '@skeletonlabs/skeleton'
+  import { getAppContext } from '../appContext'
+  import type { UserCurrent, UserSelect } from '../types'
+
+  export let user: UserSelect | UserCurrent
+
+  const app = getAppContext()
+</script>
+
+<Avatar
+  border="border-2 border-surface-400-500-token"
+  src="{app.pb.files.getUrl(user, user.avatar, { thumb: '64x0' })}}"
+  initials={user.username}
+/>
