987750ad668c91d6f4a7dffac295095dd42f1d81
Daniel Reszka <dannn.r@gmail.com>
2023-11-16 20:07:00 +0100

    utils: add date + string

---
+++ src/routes/pocketbase/utils/string.ts
@@ -0,0 +1,27 @@
+export function truncate(str: string, maxLength = 20, ellipsis = true) {
+  if (str.length <= maxLength) return str
+  return str.slice(0, maxLength) + (ellipsis ? '...' : '')
+}
+
+export function upperFirst(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+export function slugify(string: string) {
+  return string
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)+/g, '')
+}
+
+export function sanitizeFormInput(str: string) {
+  return upperFirst(str.replace(/\s+/g, ' ').trim())
+}
+
+export function stripHtml(html: string): string {
+  const tmp = document.createElement('DIV')
+  tmp.innerHTML = html
+  return tmp.textContent || tmp.innerText || ''
+}
