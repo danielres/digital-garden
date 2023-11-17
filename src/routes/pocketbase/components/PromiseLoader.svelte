9e028cae91ff3d3933df5aae8352500f867e823f
Daniel Reszka <dannn.r@gmail.com>
2023-11-17 15:54:26 +0100

    PromiseLoader: add

---
+++ src/routes/pocketbase/components/PromiseLoader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Card from './Card.svelte'
+  import Spinner from './Spinner.svelte'
+
+  type T = $$Generic
+  export let promise: Promise<T>
+  export let error = 'Resource not found'
+</script>
+
+{#await promise}
+  <Spinner />
+{:then result}
+  <slot {result} />
+{:catch _}
+  <Card variant="error">
+    <slot name="error">{error}</slot>
+  </Card>
+{/await}
