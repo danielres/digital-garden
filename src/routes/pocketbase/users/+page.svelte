1fe715808059836f88ecf00f9310070c1c774319
Daniel Reszka <dannn.r@gmail.com>
2023-11-17 15:57:35 +0100

    /users: break into modular components

--- src/routes/pocketbase/users/+page.svelte
+++ src/routes/pocketbase/users/+page.svelte
@@ -1,1 +1,7 @@
-<p>Please select a user to view details</p>
+<script lang="ts">
+  import Muted from '../components/Muted.svelte'
+</script>
+
+<Muted>
+  <p>Please select a user for details.</p>
+</Muted>
--- src/routes/pocketbase/users/[slug]/+page.svelte
+++ src/routes/pocketbase/users/[slug]/+page.svelte
@@ -1,33 +1,17 @@
 <script lang="ts">
   import { page } from '$app/stores'
   import { getAppContext } from '../../appContext'
-  import Card from '../../components/Card.svelte'
-  import type { UserSelect } from '../../types'
-  import { renderDate } from '../../utils/date'
+  import PromiseLoader from '../../components/PromiseLoader.svelte'
+  import UserFull from './../../components/UserFull.svelte'
 
   const app = getAppContext()
 
   $: slug = $page.params.slug
-
-  $: recordPromise = app.pb.collection('users').getFirstListItem(`slug="${slug}"`, {})
+  $: userPromise = app.queries.user.bySlug(slug)
 </script>
 
-{#await recordPromise}
-  <!-- recordPromise is pending -->
-{:then user}
-  <div class="grid gap-2">
-    <div>
-      <h3>{user.username}</h3>
-    </div>
-
-    <dl class="grid grid-cols-[auto_1fr] gap-x-2 text-sm opacity-75">
-      <dt>Joined:</dt>
-      <dd>{renderDate(new Date(user.created))}</dd>
-
-      <dt>Verified?</dt>
-      <dd>{user.verified ? 'yes' : 'no'}</dd>
-    </dl>
-  </div>
-{:catch error}
-  {error.message}
-{/await}
+<div class="space-y-4">
+  <PromiseLoader promise={userPromise} let:result={user} error={`User "${slug}" not found.`}>
+    <UserFull {user} />
+  </PromiseLoader>
+</div>
