38ca4c88725e8369c5980cf4d9952421c30e0cc6
Daniel Reszka <dannn.r@gmail.com>
2023-11-17 18:29:03 +0100

    /users/layout: extract UserListItem, use PromiseLoader

---
+++ src/routes/pocketbase/components/UserListItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { UserSelect } from '../types'
+  import { paths } from '../utils/navigation'
+  import Avatar from './Avatar.svelte'
+
+  export let user: UserSelect
+</script>
+
+<a
+  href={paths.users(user.slug)}
+  class="grid grid-cols-[auto_1fr] card p-4 gap-4 items-center variant-soft-surface hover:variant-glass-surface"
+>
+  <Avatar {user} />
+  <div>{user.username}</div>
+</a>
--- src/routes/pocketbase/users/+layout.svelte
+++ src/routes/pocketbase/users/+layout.svelte
@@ -1,30 +1,22 @@
 <script lang="ts">
   import { getAppContext } from '../appContext'
-  import Avatar from '../components/Avatar.svelte'
   import Card from '../components/Card.svelte'
+  import PromiseLoader from '../components/PromiseLoader.svelte'
   import type { UserSelect } from '../types'
-  import { paths } from '../utils/navigation'
+  import UserListItem from './../components/UserListItem.svelte'
 
   const app = getAppContext()
 
-  const recordsPromise = app.pb.collection<UserSelect>('users').getFullList({ sort: '-created' })
+  const usersPromise = app.pb.collection<UserSelect>('users').getFullList({ sort: '-created' })
 </script>
 
 <div class="grid grid-cols-[1fr_2fr] gap-2">
-  <div>
-    {#await recordsPromise then records}
-      <div class="space-y-2">
-        {#each records as record}
-          <a
-            href={paths.users(record.slug)}
-            class="grid grid-cols-[auto_1fr] card p-4 gap-4 items-center variant-soft-surface hover:variant-glass-surface"
-          >
-            <Avatar user={record} />
-            <div>{record.username}</div>
-          </a>
-        {/each}
-      </div>
-    {/await}
+  <div class="space-y-2">
+    <PromiseLoader promise={usersPromise} let:result={users}>
+      {#each users as user}
+        <UserListItem {user} />
+      {/each}
+    </PromiseLoader>
   </div>
 
   <Card>
