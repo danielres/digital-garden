8ceb93c20cbc603609c32c150cedfa1bb36ba7d7
Daniel Reszka <dannn.r@gmail.com>
2023-11-17 12:04:07 +0100

    Card: fix for twMerge not recognizing skeleton variants

--- src/routes/pocketbase/components/Card.svelte
+++ src/routes/pocketbase/components/Card.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { twMerge } from 'tailwind-merge'
 
+  let _class = ''
+  export { _class as class }
   export let variant: 'primary' | 'error' | 'neutral' = 'neutral'
   export let bg = true
 </script>
@@ -8,10 +10,11 @@
 <div
   class={twMerge(
     'card p-4',
-    'variant-glass-surface text-surface-700-200-token',
+    variant === 'neutral' && 'variant-glass-surface text-surface-700-200-token',
     variant === 'error' && 'variant-glass-error text-error-700-200-token',
     variant === 'primary' && 'variant-glass-primary text-primary-700-200-token',
-    !bg && '!bg-transparent'
+    !bg && '!bg-transparent',
+    _class
   )}
 >
   <slot />
