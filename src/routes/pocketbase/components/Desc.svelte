1c2aa51bb58117c3421738bc122c0bd6fa5bd3ac
Daniel Reszka <dannn.r@gmail.com>
2023-11-22 20:45:50 +0100

    Desc: add

---
+++ src/routes/pocketbase/components/Desc.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { twMerge } from 'tailwind-merge'
+  import { stripHtml, truncate } from '../utils/string'
+  import Muted from './Muted.svelte'
+
+  export let text: string
+  export let preview = false
+</script>
+
+<div
+  class={twMerge(
+    'rounded-xs variant-soft opacity-80',
+    preview ? 'text-xs px-2 py-1' : 'text-sm space-y-2 p-4'
+  )}
+>
+  {#if text}
+    {#if preview}
+      {truncate(stripHtml(text))}
+    {:else}
+      {@html text}
+    {/if}
+  {:else}
+    <Muted>No description</Muted>
+  {/if}
+</div>
