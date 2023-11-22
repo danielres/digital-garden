3fe524f52d09a292d14ad53ff8e3cb2f8548e1ad
Daniel Reszka <dannn.r@gmail.com>
2023-11-22 20:46:21 +0100

    Avatar: make more versatile

--- src/routes/pocketbase/components/Avatar.svelte
+++ src/routes/pocketbase/components/Avatar.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
   import { Avatar } from '@skeletonlabs/skeleton'
+  import { twMerge } from 'tailwind-merge'
   import { getAppContext } from '../appContext'
-  import type { UserCurrent, UserSelect } from '../types'
+  import type { CollectionName } from '../types'
 
-  export let user: UserSelect | UserCurrent
+  export let border = false
+  export let sm = false
+  export let md = false
+
+  export let record: {
+    id: string
+    avatar?: string
+    label?: string
+    collectionName?: CollectionName
+    username?: string
+  }
+
+  const params = {
+    id: record.id,
+    collectionName: record.collectionName || 'users',
+  }
+  const initials =
+    'label' in record ? record.label : 'username' in record ? record.username : undefined
 
   const app = getAppContext()
 </script>
 
 <Avatar
-  width="w-12"
-  border="border-2 border-surface-500-400-token"
-  src="{app.pb.files.getUrl(user, user.avatar, { thumb: '64x0' })}}"
-  initials={user.username}
-  shadow="shadow-md"
+  class={twMerge('aspect-square', border && 'border-2 border-surface-400-500-token')}
+  width={sm ? 'w-6' : md ? 'w-9' : 'w-12'}
+  src={record.avatar
+    ? app.pb.files.getUrl(params, record.avatar || '', { thumb: '64x0' })
+    : undefined}
+  {initials}
 />
