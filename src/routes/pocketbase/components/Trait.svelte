612195e4981825b62e1f2d3d6ca6aa46f1edb3b4
Daniel Reszka <dannn.r@gmail.com>
2023-11-22 15:20:54 +0100

    Trait: truncate desc

--- src/routes/pocketbase/components/Trait.svelte
+++ src/routes/pocketbase/components/Trait.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { Trait } from '../types'
   import { paths } from '../utils/navigation'
-  import { upperFirst } from '../utils/string'
+  import { truncate, upperFirst } from '../utils/string'
   import Muted from './Muted.svelte'
 
   export let trait: Trait
@@ -16,11 +16,11 @@
 
   <dd>{upperFirst(trait.kind)}: {trait.level}</dd>
 
-  <dd class="rounded-xs variant-soft px-2 py-1">
+  <div class="rounded-xs variant-soft px-2 py-1 text-xs opacity-80">
     {#if trait.desc}
-      {trait.desc}
+      {truncate(trait.desc)}
     {:else}
       <Muted>No description</Muted>
     {/if}
-  </dd>
+  </div>
 </dl>
